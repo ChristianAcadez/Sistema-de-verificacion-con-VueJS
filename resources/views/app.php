05e50b629e30fa10f2fb6e4ab63baafec9b25691
ChristianAcadez <christian.castllo.hernandez@gmail.com>
2021-08-30 11:01:21 -0500

    Seccond commit

---
+++ resources/views/app.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>VueAuth</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- UIKit JS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/css/uikit.min.css" />
+</head>
+<body>
+
+    <nav class="uk-navbar-container" uk-navbar>
+        <div class="uk-navbar-left">
+            <ul class="uk-navbar-nav">
+                <li class="uk-active"><a href="">VueAuth</a></li>
+                <li class="uk-parent"><a href="">Acceso</a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </div>
+    </nav>
+
+
+
+    <!-- UIKit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit-icons.min.js"></script>
+</body>
+</html>
